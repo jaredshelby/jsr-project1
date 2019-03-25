a9abbb317d7d1d496c1afaff0db53f0723052d8c
jaredshelby <jaredshelby@Jareds-MacBook-Pro.local>
2019-03-25 15:28:44 -0700

    First chat functionality

--- js/main.js
+++ js/main.js
@@ -1,7 +1,32 @@
-// add an event listener to the form to submit Dave's message
+// add an event listener to the form to submit message
+const chatInput = document.querySelector('#chatInput');
+const fullChat = document.querySelector('#fullChat');
 
-// create a function for HAL to respond to Dave's messages with variable logic based upon
-// Dave's inputs
+var currentValue = "";
+
+
+// Listener for enter key from user
+chatInput.addEventListener("keydown", function (e) {
+    if (e.keyCode === 13) {  //checks whether the pressed key is "Enter"
+      e.preventDefault();
+    	currentValue = chatInput.value;
+    	console.log(`${currentValue} in function`);
+    	chatInput.placeholder = ""
+    	// return currentValue;
+    	appendMessage(currentValue);
+    }
+});
+
+console.log(currentValue);
+
+// Adding user-entered chat to main div
+function appendMessage(chatMessage) {
+	fullChat.innerHTML += `<div><p>${currentValue}</p></div>`;
+}
+
+
+// create a function to respond to user messages with variable logic based upon inputs
+
 
 // create a function for HAL to open the chat with "Good morning, Dave"
 
